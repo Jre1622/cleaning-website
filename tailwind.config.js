298d506db32f2242434360aeeedfead151819b20
userName <jre1622@gmail.com>
2025-04-06 14:03:46 +0700

    x

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./views/**/*.ejs", // scan all .ejs files in views folder
+    "./public/**/*.{js,css,html}", // scan other relevant files
+  ],
+  plugins: [],
+};
